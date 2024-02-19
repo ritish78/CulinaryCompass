616dcf7f786f1b9e0816c4a76e87b686d9f1dbc3
ritish78 <ritishtimalsina78@gmail.com>
2024-02-19 12:59:29 +0545

    Added meals-grid and meal-item component

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,12 +1,26 @@
 import Link from "next/link";
 
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+
 export default function MealPage() {
     return (
         <>
-            <p>
-                Select any of the meals to view their recipe.
-            </p>
-            <Link href="/meals/share">Share!</Link>
+            <header className={classes.header}>
+                <h1>
+                    Exquisite Culinary Creations Crafted{' '}
+                    <span className={classes.highlight}>by You!</span>
+                </h1>
+                <p>
+                    Select your preferred recipe and prepare it either for personal enjoyment or for sharing with others. The experience is both enjoyable and fulfilling. 
+                </p>
+                <p className={classes.cta}>
+                    <Link href="/meals/share">Share your favourite recipe!</Link>        
+                </p>
+            </header>
+            <main className={classes.main}>
+                <MealsGrid meals={[]} />
+            </main>
         </>
     )
 }
